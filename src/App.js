68608c5e0bc3999aa289b74c8c29581521bc888a
dhfernando <hdilshan.fernando11@gmail.com>
2020-08-06 23:24:31 +0530

    transfering Eth

--- src/App.js
+++ src/App.js
@@ -14,20 +14,43 @@ function App() {
   const loadBlockChainData = async () => {
       const provider = new Web3.providers.HttpProvider('http://localhost:7545')
       const web3 = new Web3(Web3.givenProvider || provider);  
-
       const MyContract = require('./smartContract/build/contracts/EthReact.json')
 
-
       const id = await web3.eth.net.getId()
+
       const deployeNetwork = MyContract.networks[id];
 
       const contract = new web3.eth.Contract(
           MyContract.abi,
+
+          // smartcontract address
           deployeNetwork.address,
       )
+      // account addresses
       const addresses = await web3.eth.getAccounts()
-      const result = await contract.methods.getData().call()
-      console.log(result)
+      console.log(addresses)
+      
+      const result1 = await contract.methods.getData2().call()
+      console.log(result1);
+
+      console.log(await contract.methods.setData(addresses[4])
+        .send({ from : addresses[3]  , value : web3.utils.toWei('1', 'ether') }))
+      
+       
+      
+      const result2 = await contract.methods.getData().call()
+      console.log(result2);
+     
+      console.log(deployeNetwork.address)
+
+
+
+      web3.eth.getBalance(addresses[3] , (err , bal)=> {
+        console.log(web3.utils.fromWei(bal , 'ether'));
+      })
+      web3.eth.getBalance(addresses[4] , (err , bal)=> {
+        console.log(web3.utils.fromWei(bal , 'ether'));
+      })
   }
 
   return (
--- src/smartContract/contracts/EthReact.sol
+++ src/smartContract/contracts/EthReact.sol
@@ -2,14 +2,19 @@
 pragma solidity >= 0.5.0 < 0.7.0;
 
 contract EthReact {
-  // constructor() public {
+    string addr;
     
-  // }
+   constructor(string memory _addr) public {
+    addr = _addr;
+   }
 
 
     uint data =9 ;
-    function getData() external view returns(uint){ return data; }
-    function setData(uint _data) external { data = _data; }
+    function getData() external   view  returns (uint){ return data; }
+    function getData2() external view returns( string memory){ return addr; }
+
+    function setData(address payable _toAdd) external payable { _toAdd.transfer(msg.value);  }
+
     function setDataPrivate(uint _data) private { data = _data + 10 ; }
 
 }
